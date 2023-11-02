7a23916a18c50f84fd2999240079a1f682a0102b
Grace Tumusanganire <gracetumusanganire@gmail.com>
2023-11-02 16:07:16 +0100

    Updates

--- src/app/features/generateSurvey/generate-survey-task.ts
+++ src/app/features/generateSurvey/generate-survey-task.ts
@@ -1,34 +1,35 @@
-// import { DispatchableEvent, Task, TaskOutcome, TaskParams } from "@awarns/core/tasks";
-// import {AppComponent} from "../app.component";
-// import { notificationsManager } from '@awarns/notifications'; 
+import { DispatchableEvent, Task, TaskOutcome, TaskParams } from "@awarns/core/tasks";
+import { notificationsManager } from '@awarns/notifications'; 
 // import { Frame } from 'tns-core-modules/ui/frame';
+import { Router } from '@angular/router';
 
-// const EVENT = 'displaySurvey';
+const EVENT = 'displaySurvey';
 // const navigationOptions = {
 //   moduleName: 'survey', 
 //   clearHistory: true, 
 // };
-// export class GenerateSurveyTask extends Task {
+export class GenerateSurveyTask extends Task {
 
   
-//   constructor() {
-//     super('generateSurveyPage', {
-//       outputEventNames: ['displaySurvey']
-//     });
-//   }
+  constructor(private router: Router) {
+    super('generateSurveyPage', {
+      outputEventNames: ['displaySurvey']
+    });
+  }
 
-//  protected async onRun(taskParams: TaskParams, invocationEvent: DispatchableEvent): Promise<void | TaskOutcome> {
-//   notificationsManager.onNotificationTap((notification) => {
-//     if (notification.tapAction.type === 'DELIVER_QUESTIONS') {
-//       setTimeout(() => {
-//         Frame.topmost().navigate(navigationOptions); 
-//       }, 2);
-//     }
-//   })
+ protected async onRun(taskParams: TaskParams, invocationEvent: DispatchableEvent): Promise<void | TaskOutcome> {
+  notificationsManager.onNotificationTap((notification) => {
+    if (notification.tapAction.type === 'DELIVER_QUESTIONS') {
+        this.router.navigate(['/survey']).catch((error) => {
+            console.error('Navigation error:', error);
+  })
 
-//   return {
-//     eventName: EVENT
-//   };
-// }
-// }
+}
+  return {
+     eventName: EVENT
+   
+  };
 
+ })
+}
+}
